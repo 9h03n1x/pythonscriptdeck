5334e4e19ea6b331fa22287493f6c9534249c136
Nicco.Hagedorn <nicco.hagedorn@corsair.com>
2025-02-27 10:04:33 +0100

    updated log level

--- src/plugin.ts
+++ src/plugin.ts
@@ -4,7 +4,7 @@ import { PythonScript } from "./actions/python-script";
 import { PythonService } from "./actions/python-service";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
-streamDeck.logger.setLevel(LogLevel.TRACE);
+streamDeck.logger.setLevel(LogLevel.INFO);
 
 // Register the increment action.
 streamDeck.actions.registerAction(new PythonScript());
