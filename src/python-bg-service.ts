65e4ce390914290f1ca66fdc490524c83050495a
Nicco.Hagedorn <nicco.hagedorn@corsair.com>
2025-03-05 15:15:03 +0100

    fixed the cancel issue #5

--- src/python-bg-service.ts
+++ src/python-bg-service.ts
@@ -22,7 +22,7 @@ const pythonErrorMap: { [key: string]: string } = {
 	"OverflowError": "Python\nOverflow\nError",
 	"SystemError": "Python\nSystem\nError",
 	"Microsoft Store": "Python\nnot found\nError",
-	
+
 };
 enum ServiceState {
 	running,
@@ -32,71 +32,76 @@ enum ServiceState {
 
 class PythonBackgroundService {
 
-    trackedActions: any[] = [];
+	trackedActions: any[] = [];
 	state = ServiceState.stopped;
 
 
-    registerAction(ev: WillAppearEvent<PythonServiceSettings> | DidReceiveSettingsEvent<PythonServiceSettings>) {
-        
-        var duplicate = false;
-        streamDeck.logger.info("checking if action is already tracked");
-        var newtrackedAction = {
-            "id": ev.action.id, "ev": ev, "timer": undefined
-        }
+	registerAction(ev: WillAppearEvent<PythonServiceSettings> | DidReceiveSettingsEvent<PythonServiceSettings>) {
 
-        for (let i = 0; i < this.trackedActions.length; i++) {
-            if (this.trackedActions[i] == ev.action.id) {
-                duplicate = true;
-                streamDeck.logger.info("action already tracked - updating action");
-                clearInterval(this.trackedActions[i].timer.id);
-                this.trackedActions[this.trackedActions.indexOf(this.trackedActions[i])]= newtrackedAction;
-                break;
-            }
-            }
+		var duplicate = false;
+		streamDeck.logger.info("checking if action is already tracked");
+		var newtrackedAction = {
+			"id": ev.action.id, "ev": ev, "timerId": undefined
+		}
 
-        if (duplicate == false) {
-        this.trackedActions.push(newtrackedAction)}
-    }
-
-    unregisterAction(ev: WillDisappearEvent<PythonServiceSettings>) {
 		for (let i = 0; i < this.trackedActions.length; i++) {
-            if (this.trackedActions[i] == ev.action.id) {
-                streamDeck.logger.info(`stopping execution of the action ${ev.action.manifestId}, id: ${ev.action.id}`);
-                clearInterval(this.trackedActions[i].timer.id);}
+			if (this.trackedActions[i] == ev.action.id) {
+				duplicate = true;
+				streamDeck.logger.info("action already tracked - updating action");
+				clearInterval(this.trackedActions[i].timerId);
+				this.trackedActions[this.trackedActions.indexOf(this.trackedActions[i])] = newtrackedAction;
+				break;
 			}
+		}
+
+		if (duplicate == false) {
+			this.trackedActions.push(newtrackedAction)
+		}
 	}
 
-    start(ev: KeyDownEvent<PythonServiceSettings>){
-        streamDeck.logger.info("starting Background Service")
+	unregisterAction(ev: WillDisappearEvent<PythonServiceSettings>) {
 		for (let i = 0; i < this.trackedActions.length; i++) {
-            this.trackedActions[i].timer = this.createTimer(this.trackedActions[i].ev);
-        }
+			if (this.trackedActions[i] == ev.action.id) {
+				streamDeck.logger.info(`stopping execution of the action ${ev.action.manifestId}, id: ${ev.action.id}`);
+				clearInterval(this.trackedActions[i].timerId);
+			}
+		}
+	}
+
+	start(ev: KeyDownEvent<PythonServiceSettings>) {
+		streamDeck.logger.info("starting Background Service")
+		for (let i = 0; i < this.trackedActions.length; i++) {
+			this.trackedActions[i].timerId = this.createTimer(this.trackedActions[i].ev);
+		}
 		this.state = ServiceState.running;
 		ev.action.setImage("imgs/actions/pyServiceRunning.png");
-    }
+	}
 
-    stop(ev: KeyDownEvent<PythonServiceSettings>){
-        streamDeck.logger.info("stopping Background Service")
+	stop(ev: KeyDownEvent<PythonServiceSettings>) {
+		streamDeck.logger.info("stopping Background Service")
 		for (let i = 0; i < this.trackedActions.length; i++) {
-			if(this.trackedActions[i].timer == undefined){
+			if (this.trackedActions[i].timerId == undefined) {
 				continue;
-			}else{clearInterval( this.trackedActions[i].timer.id);}
-            
-			this.trackedActions[i].timer = undefined;
-        }
+			} else {
+				clearInterval(this.trackedActions[i].timerId);
+				this.trackedActions[i].timerId = undefined;
+			}
+
+
+		}
 		this.state = ServiceState.stopped;
 		streamDeck.logger.info(`stopping execution of the action ${ev.action.manifestId}, id: ${ev.action.id}`)
 		ev.action.setImage("imgs/actions/pyServiceStopped.png");
-    }
+	}
 
 	getState = () => {
 		return this.state;
 	}
 
-    
 
-    executeAction(ev: WillAppearEvent<PythonServiceSettings> | DidReceiveSettingsEvent<PythonServiceSettings> |KeyDownEvent<PythonServiceSettings>){
-        const settings = ev.payload.settings;
+
+	executeAction(ev: WillAppearEvent<PythonServiceSettings> | DidReceiveSettingsEvent<PythonServiceSettings> | KeyDownEvent<PythonServiceSettings>) {
+		const settings = ev.payload.settings;
 		const { path } = settings;
 		let pythonProcess: ChildProcess | undefined;
 		if (path) {
@@ -114,7 +119,7 @@ class PythonBackgroundService {
 					else if (settings.image2 && (data.toString().trim() == (settings.value2 ?? ""))) {
 						ev.action.setImage(settings.image2)
 
-					}else(
+					} else (
 						ev.action.setImage("imgs/actions/pyServiceIcon.png")
 					)
 				});
@@ -130,7 +135,7 @@ class PythonBackgroundService {
 							break;
 						}
 					}
-					if (errorTitle == "python\nother\nissue"){
+					if (errorTitle == "python\nother\nissue") {
 						streamDeck.logger.error(errorString);
 					}
 					ev.action.setTitle(errorTitle);
@@ -144,8 +149,8 @@ class PythonBackgroundService {
 			}
 		}
 
-    }
-    createChildProcess(useVenv: boolean, venvPath: string | undefined, path: string) {
+	}
+	createChildProcess(useVenv: boolean, venvPath: string | undefined, path: string) {
 		let pythonProcess: ChildProcess | undefined;
 		if (useVenv && venvPath) {
 			streamDeck.logger.debug(`Use Virtual Environment: ${venvPath}`)
@@ -154,7 +159,7 @@ class PythonBackgroundService {
 		}
 		else {
 			pythonProcess = spawn("python3", [path]);
-			if (pythonProcess.connected == false){
+			if (pythonProcess.connected == false) {
 				streamDeck.logger.debug("python3 not found, trying python")
 				pythonProcess = spawn("python", [path]);
 			}
@@ -168,12 +173,12 @@ class PythonBackgroundService {
 		return fileName;
 	}
 
-	createTimer(ev:WillAppearEvent<PythonServiceSettings> | DidReceiveSettingsEvent<PythonServiceSettings> |KeyDownEvent<PythonServiceSettings> ){
+	createTimer(ev: WillAppearEvent<PythonServiceSettings> | DidReceiveSettingsEvent<PythonServiceSettings> | KeyDownEvent<PythonServiceSettings>) {
 		const interval = ev.payload.settings.interval ?? 10
 		return setInterval(() => {
 			streamDeck.logger.info(`timer triggered after ${ev.payload.settings.interval}s for action ${ev.action.manifestId}, id: ${ev.action.id}`)
 			this.executeAction(ev);
-		}, interval*1000)
+		}, interval * 1000)
 
 	}
 
